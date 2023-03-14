ee9290462590ff714506c26ac9106214eed787b8
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-03-13 22:36:50 -0300

    Métodos atualizar e remover

--- src/main/java/br/com/fiap/meujulius/controllers/DespesaController.java
+++ src/main/java/br/com/fiap/meujulius/controllers/DespesaController.java
@@ -8,27 +8,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.meujulius.models.Despesa;
 
 @RestController
+@RequestMapping("/api/despesas")
 public class DespesaController {
 
     Logger log = LoggerFactory.getLogger(DespesaController.class);
     
     List<Despesa> despesas = new  ArrayList<>();
 
-    @GetMapping("/api/despesas")
+    @GetMapping
     public List<Despesa> index(){
         return despesas;
     }
 
-    @PostMapping("/api/despesas")
+    @PostMapping
     public ResponseEntity<Despesa> create(@RequestBody Despesa despesa){
         log.info("cadastrando despesa: " + despesa);
         despesa.setId(despesas.size() + 1l);
@@ -36,13 +40,35 @@ public class DespesaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(despesa);
     }
 
-    @GetMapping("/api/despesas/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Object> show(@PathVariable Long id){
         log.info("buscando despesa com id " + id);
         Optional<Despesa> optionalDespesa = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
         if (optionalDespesa.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Despesa não encontrada");
         return ResponseEntity.ok(optionalDespesa.get());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id){
+        log.info("apagando despesa com id " + id);
+        Optional<Despesa> optionalDespesa = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
+        if (optionalDespesa.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Despesa não encontrada");
+        despesas.remove(optionalDespesa.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Despesa despesa){
+        log.info("apagando despesa com id " + id);
+        Optional<Despesa> optionalDespesa = despesas.stream().filter(d -> d.getId().equals(id)).findFirst();
+        if (optionalDespesa.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Despesa não encontrada");
+        despesas.remove(optionalDespesa.get());
+        despesa.setId(id);
+        despesas.add(despesa);        
+        return ResponseEntity.ok(despesa);
     }
 
 }
